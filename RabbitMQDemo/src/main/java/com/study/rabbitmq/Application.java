5211fc1e1ba736bf20431ff03f95fbe759d78912
yule <Yl3628691*>
2018-09-21 14:21:00 +0800

    project init

---
+++ RabbitMQDemo/src/main/java/com/study/rabbitmq/Application.java
@@ -0,0 +1,19 @@
+package com.study.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: yule
+ * @Date: 2018/9/21 0021 11:36
+ * @Description:
+ */
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
